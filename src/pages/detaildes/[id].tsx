a82fd80365ca7cd126fde008af78f8bb07c6491b
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-03-20 00:46:16 +0900

    ✨ feat: Api 연결 시 정보를 받아오지 못 했을 때 토스트 알람창 기능 구현 추가

--- src/pages/detaildes/[id].tsx
+++ src/pages/detaildes/[id].tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import DiseaseDetail from '@/app/_component/temp/DiseaseDetail';
 import MainHeader from "@/app/_component/molecule/BackHeader";
 import {diseaseList} from "@/utils/disease-api";
+import Toast from '@/app/_component/atom/Toast';
 
 export default function DetailDesPage() {
   const router = useRouter();
   const { id } = router.query;
   const disease = diseaseList.find((d) => d.id === Number(id));
+  const [isToastOpen, setIsToastOpen] = useState(false);
 
-  if (!disease) {
-    return <p>감염병 정보를 찾을 수 없습니다.</p>;
-  }
+  useEffect(() => {
+    if (!disease) {
+      setIsToastOpen(true);
+      setTimeout(() => {
+        setIsToastOpen(false);
+        router.push('/vaclookup');
+      }, 3000);
+    }
+  }, [disease, router]);
 
   return (
     <>
       <MainHeader title="감염병 정보" url="/vaclookup" />
-      <DiseaseDetail disease={disease} />
+      {disease ? (
+        <DiseaseDetail disease={disease} />
+      ) : (
+        <Toast message="감염병 정보를 찾을 수 없습니다." isOpen={isToastOpen} />
+      )}
     </>
   );
 }
