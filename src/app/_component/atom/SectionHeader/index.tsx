c8164ea1fdb1144d537f1b1e562a2320144cd19c
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-03-19 12:17:00 +0900

    💬 delete: 텍스트 제거

--- src/app/_component/atom/SectionHeader/index.tsx
+++ src/app/_component/atom/SectionHeader/index.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { SectionHeaderContainer, Section } from './styles';
 import { SectionHeaderProps } from '@/app/_component/atom/atomType';
 
-
 // 이 헤더 가져다 쓰는 상위 컴포넌트에서 세션 텍스트만 넣어주면 자동으로 됩니다!
 // 그 텍스트는 배열에 작성하면 됩니다!
 // 예시  : const sectionTexts = ["필수예방접종", "국가예방접종", "기타예방접종"];
--- src/app/_component/atom/SectionHeader/styles.ts
+++ src/app/_component/atom/SectionHeader/styles.ts
@@ -9,15 +9,13 @@ export const SectionHeaderContainer = styled.div`
 export const Section = styled.div`
   flex: 1;
   padding: 10px 20px;
-  border-bottom: 1px solid #e5e8eb; /* 기본값 */
-  color: #8b95a1; /* 기본값 */
+  border-bottom: 1px solid #e5e8eb;
+  color: #8b95a1;
   font-family: 'Pretendard', sans-serif;
   font-size: 16px;
-  font-weight: 400; /* 기본값 */
+  font-weight: 400;
   line-height: 32px;
   text-align: center;
   cursor: pointer;
   transition: all 0.3s;
-
-  /* 선택된 섹션에 대한 스타일은 컴포넌트 내에서 조건부로 적용 */
 `;
