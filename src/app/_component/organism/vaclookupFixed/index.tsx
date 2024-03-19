9ff77453e17b3e0f49f087e64c296b9d39f746d6
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-03-19 12:09:11 +0900

    🚑 hotfix: information 컴포넌트 타입 로직을 props로 수정

--- src/app/_component/organism/vaclookupFixed/index.tsx
+++ src/app/_component/organism/vaclookupFixed/index.tsx
@@ -3,12 +3,6 @@ import MainHeader from '@/app/_component/atom/MainHeader';
 import SectionHeader from '@/app/_component/atom/SectionHeader';
 import Information from '@/app/_component/atom/Information';
 
-interface VacLookupFixedProps {
-  selectedSection: string;
-  handleSectionChange: (section: string) => void;
-  messages: { [key: string]: string };
-}
-
 const VacLookupFixed: React.FC<VacLookupFixedProps> = ({ selectedSection, handleSectionChange, messages }) => {
   const sectionTexts = ["필수예방접종", "국가예방접종", "기타예방접종"];
 
@@ -21,19 +15,18 @@ const VacLookupFixed: React.FC<VacLookupFixedProps> = ({ selectedSection, handle
         containerProps={{
           background: '#F2F4F6',
           padding: '15px 20px',
-          gap: '14px'
+          gap: '14px',
+          height: '70px', // Default height
         }}
         iconProps={{
           color: '#6B7684',
           marginRight: '10px'
         }}
         textProps={{
-          fontFamily: 'Pretendard, sans-serif',
           fontSize: '12px',
           fontWeight: 500,
           lineHeight: '20px',
           letterSpacing: '-0.03em',
-          textAlign: 'left',
           color: '#6B7684'
         }}
       />
