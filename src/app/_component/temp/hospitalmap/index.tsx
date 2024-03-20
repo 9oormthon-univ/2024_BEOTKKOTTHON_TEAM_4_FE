117b01202acbd1661c93f882f6234cefeb7d8633
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-03-20 20:56:16 +0900

    ♻️ refactor: 재조회 버튼 컴포넌트 분리

--- src/app/_component/temp/hospitalmap/index.tsx
+++ src/app/_component/temp/hospitalmap/index.tsx
@@ -3,8 +3,7 @@ import styled from '@emotion/styled';
 import { hospitals } from '@/utils/influ-api';
 import Tooltip from '@/app/_component/atom/Tooltip';
 import { Modal } from '../../atom/MapModal';
-import { Images } from '@globalStyles';
-import Image from 'next/image';
+import ReloadButton from '@/app/_component/atom/ReloadButton';
 
 const Main = styled.div`
   display: flex;
@@ -14,36 +13,6 @@ const Main = styled.div`
   padding: 6rem;
   min-height: 100vh;
   margin-top: -110px;
-`;
-
-const CurrentLocationButton = styled.div`
-  position: absolute;
-  left: 30%;
-  top: 15px;
-  width: 150px;
-  height: 40px;
-  align-items: center;
-  padding: 8px 16px;
-  background-color: #ffffff;
-  color: #000000;
-  border: none;
-  border-radius: 20px;
-  cursor: pointer;
-  z-index: 5;
-  gap: 12px;
-  font-family: Pretendard;
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 16.8px;
-  text-align: center;
-`;
-
-const ClickableArea = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
 `;
 
 export default function HospitalMap() {
@@ -134,18 +103,9 @@ export default function HospitalMap() {
       <div id="map" style={{ width: '160%', height: '500px' }}>
         {!isMapLoaded && <p>지도를 준비 중입니다!</p>}
         <Tooltip />
-        <CurrentLocationButton>
-          <ClickableArea onClick={handleCurrentLocationClick}>
-            <Image src={Images.ico_map_reload} alt={'현재 위치 재조회'} />
-            현재 위치 재검색
-          </ClickableArea>
-        </CurrentLocationButton>
+        <ReloadButton onClick={handleCurrentLocationClick} />
       </div>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        content={modalContent}
-      />
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} content={modalContent} />
     </Main>
   );
 }
