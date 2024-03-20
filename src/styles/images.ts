bfab434c3c5823bccf77780f6a5f35265f74cb62
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-03-20 19:57:25 +0900

    🍱 add: 백신조회 마커들 아이콘 이미지 삽입 및 타입 설정

--- src/styles/images.ts
+++ src/styles/images.ts
@@ -74,6 +74,11 @@ import ico_check_filled from '../../public/assets/ico/ico-check-filled.svg';
 import ico_pin_selec from '../../public/assets/ico/ico-pin-selec.svg';
 import ico_pin_unselec from '../../public/assets/ico/ico-pin-unselec.svg';
 
+//백신 지도에 들어가는 마커 이미지들
+import ico_map_my from '../../public/assets/ico/ico-map-my.svg';
+import ico_map_selec from '../../public/assets/ico/ico-map-selec.svg';
+import ico_map_unselec from '../../public/assets/ico/ico-map-unselec.svg';
+
 // prettier-ignore
 export const Images: ImagesType = {
   // icon
@@ -137,6 +142,11 @@ export const Images: ImagesType = {
   arrow_left: arrow_left,
   dropdown_up: dropdown_up,
   dropdown_down: dropdown_down,
+
+  ico_map_my:ico_map_my,
+  ico_map_selec:  ico_map_selec,
+  ico_map_unselec: ico_map_unselec,
+
 
   //image
   vacgom01: Vacgom01,
