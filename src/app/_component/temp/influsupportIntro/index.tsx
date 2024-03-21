68d7eadd0f1edf64fea02f84ffd1cf9848eaced9
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-03-21 19:35:17 +0900

    🐛 fix: 지원 사업 마진 값 수정

--- src/app/_component/temp/influsupportIntro/index.tsx
+++ src/app/_component/temp/influsupportIntro/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Image from 'next/image';
 import { Images } from '@globalStyles';
 
+const MainSection = styled.div`
+  width: 393px;
+  min-height: 100vh;
+  padding: 20px 20px 0px 20px;
+  margin-top: -20px;
+  gap: 0px;
+  opacity: 0px;
+`;
+
 const SupportIntro = () => {
   return (
-    <>
-      <Image src={Images.ico_support_intro} alt=""  />
-    </>
+    <MainSection>
+      <Image src={Images.ico_support_intro} alt="" />
+    </MainSection>
   );
 };
 
