4e9076397c75236a9cacd041ad4cce61adb1e1b8
minjeoong <orange_mj@naver.com>
2024-03-22 22:17:27 +0900

    :sparkles: feat: [회원가입] 에러수정

--- src/app/signup/captcha/page.tsx
+++ src/app/signup/captcha/page.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import * as React from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-
+import { useRouter } from 'next/navigation';
 import { VerificationWrap } from './style';
 import Image from 'next/image';
-
 import VerificationInput from '../../_component/atom/verificationInput';
 import BackHeader from '@/app/_component/molecule/BackHeader';
 import BottomButton from '@/app/_component/atom/BottomButton';
-import { postSignup } from '@/app/_lib/postSignup';
 import { postchallenge } from '@/app/_lib/postchallenge';
 import { LocalStorage } from '@/hooks/useUtil';
 
@@ -18,9 +15,10 @@ export default function Captcha(): React.JSX.Element {
   const router = useRouter();
   let secureNoImage = LocalStorage.getItem('secureNoImage');
 
+  console.log(secureNoImage);
+
   const handleNextButtonClick = async () => {
     if (password.length >= 5) {
-      router.push('/signup/verification');
       try {
         const response = await postchallenge(password);
         console.log('Signup successful:', response);
@@ -30,8 +28,6 @@ export default function Captcha(): React.JSX.Element {
       }
     }
   };
-  const onClickRefresh = () => {};
-
   return (
     <VerificationWrap>
       <BackHeader title={'예방접종도우미 회원가입'} url={'/signup/more'} />
