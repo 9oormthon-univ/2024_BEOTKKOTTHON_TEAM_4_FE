9f35da29eee05fce8d98a923cd242dfc653a727c
minjeoong <orange_mj@naver.com>
2024-03-23 22:14:07 +0900

    :sparkles: feat: 백신 아이템 overflow 막음

--- src/app/vachistory/page.tsx
+++ src/app/vachistory/page.tsx
@@ -13,14 +13,29 @@ import { MenuTitleContainer } from '@/app/_component/atom/MenuTitle/styles';
 import MenuTitle from '@/app/_component/atom/MenuTitle';
 import VaccineItem from '@/app/_component/atom/VaccineItem';
 import NavigationFixed from '@/app/_component/organism/navigationFixed';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { postVacSignup } from '@/app/_lib/postVacSignup';
 import responsJsonNation from '@/utils/user-vacList-api.json';
 import responsJsonetc from '@/utils/user-vacList-api.json';
+import { getInoculationSimple } from '@/app/_lib/getInoculationSimple';
 
 export default function Vachistory() {
-  const NationData = responsJsonNation;
-  const EtcData = responsJsonetc;
+  const [NationData, setNationData] = useState([]);
+  const [EtcData, setEtcData] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const nationData = await getInoculationSimple('nation');
+      const extraData = await getInoculationSimple('extra');
+      setNationData(nationData);
+      setEtcData(extraData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
 
   return (
     <Container>
@@ -62,7 +77,7 @@ export default function Vachistory() {
         <div className="vaccine_wrap">
           <div className="category">기타 예방접종</div>
           <div className="vaccine_list">
-            {NationData.map((item, key) => (
+            {EtcData.map((item, key) => (
               <VaccineItem
                 key={key}
                 category={'기타 예방접종'}
