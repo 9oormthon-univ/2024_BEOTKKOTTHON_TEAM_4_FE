f64af7c1a05a8ac94efdb6b226bfa165b2b6a282
minjeoong <orange_mj@naver.com>
2024-03-23 14:21:09 +0900

    :sparkles: feat: 성공시 vaccineList 저장

--- src/app/login/helper/page.tsx
+++ src/app/login/helper/page.tsx
@@ -61,6 +61,7 @@ export default function HelperLogin(): React.JSX.Element {
         SecureLocalStorage.setItem('password', params.password);
         if (response.success) {
           LocalStorage.setItem('type', 'loginEnd');
+          LocalStorage.setItem('vaccineList', response.vaccineList);
           router.push(`/signup/done`);
         } else {
           setError(response.message);
