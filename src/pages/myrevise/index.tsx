a2c89f68d7ad05e2f1cae17ebe47a902dddbf2b8
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-03-23 19:53:55 +0900

    ✨ feat: 마이페이지 내 정보 수정 api 연결

--- src/pages/myrevise/index.tsx
+++ src/pages/myrevise/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import MainHeader from '@/app/_component/atom/RouteHeader';
 import NavigationFixed from '@/app/_component/organism/navigationFixed';
@@ -16,7 +16,7 @@ const ImageWrapper = styled.div`
   opacity: 1;
 `;
 
-const ContextContainer= styled.div`
+const ContextContainer = styled.div`
   margin-bottom: 60px;
 `;
 const FormSection = styled.div`
@@ -29,7 +29,7 @@ const FormItemLabel = styled.label`
   font-weight: 600;
   line-height: 16.71px;
   text-align: left;
-  color: #333D4B;
+  color: #333d4b;
   display: block;
   margin-bottom: 10px;
 `;
@@ -39,10 +39,10 @@ const FormDisplay = styled.div`
   height: 56px;
   padding: 0 20px;
   border-radius: 8px;
-  border: 1px solid #E5E8EB;
-  background-color: #F9FAFB;
-  font-color: #8B95A1;
-  color: #333D4B;
+  border: 1px solid #e5e8eb;
+  background-color: #f9fafb;
+  font-color: #8b95a1;
+  color: #333d4b;
   display: flex;
   align-items: center;
   opacity: 1;
@@ -56,65 +56,89 @@ const DropdownContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   border-radius: 8px;
-  border: 1px solid #E5E8EB;
-  background-color: #FFFFFF; 
+  border: 1px solid #e5e8eb;
+  background-color: #ffffff;
   margin-bottom: 10px;
   opacity: 1;
   color: #000000;
 `;
 
+const ButtonContainer = styled.div`
+  width: 100%;
+  height: 56px;
+  background-color: #4196fd;
+`;
+
+const ButtonText = styled.div`
+  font-color: #ffffff;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 16px;
+  letter-spacing: -0.30000001192092896px;
+  text-align: center;
+  padding: 20px;
+`;
+
 export default function Myrevise() {
+  const [isConditionModalOpen, setConditionModalOpen] = useState(false);
+  const [isPregnantModalOpen, setPregnantModalOpen] = useState(false);
+  const [isMedicalWorkerModalOpen, setMedicalWorkerModalOpen] = useState(false);
+  const [isTransplantModalOpen, setTransplantModalOpen] = useState(false);
 
-    const [isConditionModalOpen, setConditionModalOpen] = useState(false);
-    const [isPregnantModalOpen, setPregnantModalOpen] = useState(false);
-    const [isMedicalWorkerModalOpen, setMedicalWorkerModalOpen] = useState(false);
-    const [isTransplantModalOpen, setTransplantModalOpen] = useState(false);
-  
-    const conditionOptions = ["기저질환 있음", "기저질환 없음"];
-    const pregnantOptions = ["임신 중이예요", "임신 중이 아니예요"];
-    const medicalWorkerOptions = ["의료기관 종사자예요", "의료기관 종사자가 아니예요"];
-    const transplantOptions = ["장기이식 경험이 있어요", "장기이식 경험이 없어요"];
+  const conditionOptions = ['기저질환 있음', '기저질환 없음'];
+  const pregnantOptions = ['임신 중이예요', '임신 중이 아니예요'];
+  const medicalWorkerOptions = [
+    '의료기관 종사자예요',
+    '의료기관 종사자가 아니예요',
+  ];
+  const transplantOptions = [
+    '장기이식 경험이 있어요',
+    '장기이식 경험이 없어요',
+  ];
 
-    const [selectedCondition, setSelectedCondition] = useState("선택하세요");
-    const [selectedPregnant, setSelectedPregnant] = useState("선택하세요");
-    const [selectedMedicalWorker, setSelectedMedicalWorker] = useState("선택하세요");
-    const [selectedTransplant, setSelectedTransplant] = useState("선택하세요");
+  const [selectedCondition, setSelectedCondition] = useState('선택하세요');
+  const [selectedPregnant, setSelectedPregnant] = useState('선택하세요');
+  const [selectedMedicalWorker, setSelectedMedicalWorker] =
+    useState('선택하세요');
+  const [selectedTransplant, setSelectedTransplant] = useState('선택하세요');
 
-    const [userNickname, setUserNickname] = useState("");
-    const [userName, setUserName] = useState("");
-  
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState("");
-    
-    const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiNDkxOGUwOC05YzcxLTQxNWUtOWIxMC00ZmQyNWYxMDRkNzEiLCJpYXQiOjE3MTExNzI1OTUsInJvbGUiOiJST0xFX1VTRVIiLCJleHAiOjE3MjAxNzI1OTV9.V3FsYMvYqqKAV76ryZkX_2TEO9WSlR43koBWgrBcA78';
-    
-    useEffect(() => {
-      fetch('https://api-dev.vacgom.co.kr/api/v1/me', {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => {
+  const [userNickname, setUserNickname] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const accessToken =
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiNDkxOGUwOC05YzcxLTQxNWUtOWIxMC00ZmQyNWYxMDRkNzEiLCJpYXQiOjE3MTExNzI1OTUsInJvbGUiOiJST0xFX1VTRVIiLCJleHAiOjE3MjAxNzI1OTV9.V3FsYMvYqqKAV76ryZkX_2TEO9WSlR43koBWgrBcA78';
+
+  useEffect(() => {
+    fetch('https://api-dev.vacgom.co.kr/api/v1/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         setUserNickname(data.nickname);
         setUserName(data.name);
         setIsLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         setError(error.message);
         setIsLoading(false);
       });
-    }, []);
-  
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
     <div>
@@ -123,15 +147,15 @@ export default function Myrevise() {
         <Image src={Images.ico_my_profile} alt="" width={100} height={100} />
       </ImageWrapper>
       <ContextContainer>
-      <FormSection>
-        <FormItemLabel htmlFor="username">이름</FormItemLabel>
-        <FormDisplay>{userName}</FormDisplay>
-      </FormSection>
-      <FormSection>
-        <FormItemLabel htmlFor="nickname">닉네임</FormItemLabel>
-        <FormDisplay>{userNickname}</FormDisplay>
-      </FormSection>
-      <FormSection>
+        <FormSection>
+          <FormItemLabel htmlFor="username">이름</FormItemLabel>
+          <FormDisplay>{userName}</FormDisplay>
+        </FormSection>
+        <FormSection>
+          <FormItemLabel htmlFor="nickname">닉네임</FormItemLabel>
+          <FormDisplay>{userNickname}</FormDisplay>
+        </FormSection>
+        <FormSection>
           <FormItemLabel>기저 질환</FormItemLabel>
           <DropdownContainer onClick={() => setConditionModalOpen(true)}>
             <span>{selectedCondition}</span>
@@ -147,7 +171,7 @@ export default function Myrevise() {
             onReset={() => {}}
           />
         </FormSection>
-      <FormSection>
+        <FormSection>
           <FormItemLabel>임신 여부</FormItemLabel>
           <DropdownContainer onClick={() => setPregnantModalOpen(true)}>
             <span>{selectedPregnant}</span>
@@ -191,12 +215,14 @@ export default function Myrevise() {
             options={transplantOptions}
             selectedOptions={[]}
             onClose={() => setTransplantModalOpen(false)}
-            onOptionSelect={(option) =>setSelectedTransplant(option)}
+            onOptionSelect={(option) => setSelectedTransplant(option)}
             onReset={() => {}}
           />
         </FormSection>
       </ContextContainer>
-      <NavigationFixed />
+      <ButtonContainer>
+        <ButtonText> 저장하기</ButtonText>
+      </ButtonContainer>
     </div>
   );
 }
