9f590754955e1edd7ae465cb95d83dcd858f9454
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-04-14 23:28:31 +0900

    ♻️ Refactor: 질병 카드 컴포넌트 스타일 코드 리팩토링

---
+++ src/app/_component/atom/NationDiseaseCard/style.ts
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+
+export const Card = styled.div`
+  width: 200px;
+  height: auto;
+  padding: 20px 21.77px;
+  gap: 20px;
+  border-radius: 14px;
+  background: #f9fafb;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const DiseaseImage = styled.div`
+  width: 100px;
+  height: 100px;
+  gap: 0px;
+`;
+
+export const DiseaseName = styled.span`
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 19.09px;
+  text-align: center;
+  color: #333d4b;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 100%;
+`;
+
+export const SubContainer = styled.div`
+  width: 134 px;
+  height: 22px;
+  padding: 4px 7px 4px 7px;
+  gap: 10px;
+  border-radius: 7px;
+  opacity: 0px;
+  background: #4196fd;
+  color: #ffffff;
+`;
+
+export const Subtitle = styled.div`
+  font-family: Pretendard;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 14.32px;
+  text-align: center;
+`;
