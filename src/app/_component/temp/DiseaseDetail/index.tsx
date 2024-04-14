472ea23de11dd171f58a2b27cfe2b6979b1c4b9d
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-04-14 23:03:51 +0900

    ♻️ Refactor: 질병 정보 세부 페이지 스타일 분리 리팩토링

--- src/app/_component/temp/DiseaseDetail/index.tsx
+++ src/app/_component/temp/DiseaseDetail/index.tsx
@@ -1,65 +1,16 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import Image from 'next/image';
 import { Images } from '@globalStyles';
 import Accordion from '@/app/_component/atom/Accordion';
-
-const DiseaseDetailContainer= styled.div`
-  width: 100%;
-  padding: 0px, 20px, 20px, 20px;
-  margin-bottom: 100px;
-  gap:14px;
-`
-
-const DiseaseImageContainer = styled.div`
-  width: 100%;
-  height: 290px;
-  padding-top: 30px;
-  background: #f9fafb;
-  border-radius: 24px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  margin-bottom: 20px;
-`;
-
-const DiseaseInfoContainer = styled.div`
-  width: 100%;
-  padding: 30px 24px;
-  border: 1px solid #f2f4f6;
-  border-radius: 20px;
-  margin-bottom: 20px;
-`;
-
-const DiseaseName = styled.h1`
-  font-family: Pretendard;
-  font-size: 24px;
-  font-weight: 700;
-  line-height: 23px;
-  text-align: left;
-  margin-bottom: 10px;
-  color: #333d4b;
-`;
-
-const DiseaseDescription = styled.p`
-  font-family: Pretendard;
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 22px;
-  letter-spacing: -0.03em;
-  text-align: left;
-  color: #6b7684;
-`;
-
-const DiseaseNameContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-`;
-
-const Icon = styled.div`
-  margin-right: 10px;
-`;
+import {
+  DiseaseDetailContainer,
+  DiseaseImageContainer,
+  DiseaseInfoContainer,
+  DiseaseName,
+  DiseaseDescription,
+  DiseaseNameContainer,
+  Icon
+} from './styles';
 
 const DiseaseDetail = ({ disease }) => {
   return (
