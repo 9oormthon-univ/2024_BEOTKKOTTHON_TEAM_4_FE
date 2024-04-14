0631c78cc27c051ddce2457681c007314e1db541
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-04-14 23:33:32 +0900

    ♻️ Refactor: 백신이 없을 경우에 보여지는 컴포넌트에 대한 스타일 리팩토링

---
+++ src/app/_component/atom/NoneHome/styles.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const NoneContainer = styled.header`
+  width: 100%;
+  height: 140px;
+  padding: 16px, 21.77px, 16px, 21.77px
+  gap: 10px;
+  border-radius: 14px;
+  opacity: 0px;
+  background#F9FAFB;
+  text-align: center;
+  z-index: 1000;
+  background: #F9FAFB;
+`;
+
+export const Title = styled.h1`
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 16.71px;
+  text-align: center;
+  color: #8b95a1;
+`;
+
+export const ImageContainer = styled.div`
+  margin-top: 40px;
+`;
