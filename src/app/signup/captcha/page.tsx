d4bac05169b3cd098728683123b1cda5aa29014f
minjeoong <orange_mj@naver.com>
2024-05-09 03:37:15 +0900

    🐛FIX : signup/captcha 에러 핸들링

--- src/app/signup/captcha/page.tsx
+++ src/app/signup/captcha/page.tsx
@@ -1,33 +1,52 @@
 'use client';
 
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { VerificationWrap } from './style';
 import Image from 'next/image';
+//style
+import { VerificationWrap } from './style';
+import { Images } from '@/styles';
+//component
 import VerificationInput from '../../_component/atom/verificationInput';
 import BackHeader from '@/app/_component/molecule/BackHeader';
 import BottomButton from '@/app/_component/atom/BottomButton';
+import WarningToastWrap from '@/app/_component/molecule/WorningToastWrap';
+//lib
 import { postchallenge } from '@/app/_lib/postchallenge';
+
 import { LocalStorage } from '@/hooks/useUtil';
-import { Images } from '@/styles';
+//type
 import { OnChangeValueType } from '@/types/globalType';
-import { useState } from 'react';
 
 export default function Captcha(): React.JSX.Element {
   const [password, setPassword] = useState<string>(''); //현재 입력된 숫자
   const router = useRouter();
-  let secureNoImage = LocalStorage.getItem('secureNoImage');
+  const [secureNoImage, setSecureNoImage] = useState<string | null>(null); // 상태 추가
+  const [errormessage, setErrormessage] = useState(''); // 로딩 상태 추가
 
-  console.log(secureNoImage);
-  console.log(password);
+  useEffect(() => {
+    setSecureNoImage(LocalStorage.getItem('secureNoImage'));
+  }, []);
 
   const handleNextButtonClick = async () => {
     if (password && password.length >= 5) {
       try {
         const response = await postchallenge(password);
-        if (response) {
-          console.log('Signup successful:', response);
+        const { success, message, data, code } = response;
+        if (success) {
           router.push(`/signup/verification`);
+        } else {
+          setPassword('');
+          setErrormessage(message);
+          // 잘못 입력시
+          if (code === 'RETRY_SECURE_NO') {
+            setSecureNoImage(data.secureNoImage);
+          }
+          // codef 응답시간 초과 에러
+          if (code === 'CODEF_ERROR') {
+            setErrormessage(data.message);
+          }
         }
       } catch (error) {
         console.error('Signup failed:', error.message);
@@ -59,6 +78,8 @@ export default function Captcha(): React.JSX.Element {
           onChangeValue={onChangeValue}
         />
       </div>
+      <WarningToastWrap errorMessage={errormessage} />
+
       <BottomButton
         filled={password !== ''}
         handleNextButtonClick={handleNextButtonClick}
