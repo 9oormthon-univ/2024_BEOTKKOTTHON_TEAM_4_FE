7e1ea0697bc3514a15aa2bec25cdb4331e06aa06
오소현 <53892427+osohyun0224@users.noreply.github.com>
2024-05-12 01:08:13 +0900

    ♻️ refactor : 올바를때 토스트 메세지 컴포넌트 분리

--- src/app/_component/temp/influhospitalmap/index.tsx
+++ src/app/_component/temp/influhospitalmap/index.tsx
@@ -5,6 +5,7 @@ import Tooltip from '@/app/_component/atom/Tooltip';
 import { Images } from '@globalStyles';
 import { Modal } from '../../atom/MapModal';
 import ReloadButton from '@/app/_component/atom/ReloadButton';
+import CurrectToast from '../../atom/CurrectToast';
 
 const Main = styled.div`
   display: flex;
@@ -19,51 +20,6 @@ const MapContainer = styled.div`
   width: 100%;
   height: 100%;
 `;
-
-const ToastContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  bottom: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 90%;
-  height: 66px;
-  padding: 15px 20px;
-  background-color: #E5F0FF;
-  border-radius: 18px;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  opacity: 0;
-  transition: opacity 0.3s;
-  z-index: 10;
-  margin-bottom:60px
-`;
-
-const ToastImage = styled.img`
-  width: 24px;
-  height: 24px;
-  margin-right: 10px;
-  text-align: left;
-`;
-
-const ToastMessage = styled.div`
-  font-family: Pretendard;
-  font-size: 16px;
-  font-weight: 600;
-  line-height: 20px;
-  color: #4196FD;
-  text-align: left;
-`;
-
-const LocationToast = ({ isVisible }) => {
-  return (
-    <ToastContainer style={{ opacity: isVisible ? 1 : 0 }}>
-      <ToastImage src="/assets/ico/ico-checkbox-selected-enabled.svg" alt="Checked Icon" />
-      <ToastMessage>현재 위치는 성남시 분당구 입니다</ToastMessage>
-    </ToastContainer>
-  );
-};
 
 export default function HospitalMap() {
   const [isMapLoaded, setIsMapLoaded] = useState(false);
@@ -225,7 +181,7 @@ export default function HospitalMap() {
         />
       </MapContainer>
     </Main>
-    <LocationToast isVisible={showToast} />
+    <CurrectToast isVisible={showToast} />
     </>
   );
 }
