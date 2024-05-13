ee9bfb6b9944ae3bcfa73dc095e1286f48d91d81
minjeoong <orange_mj@naver.com>
2024-05-14 02:07:53 +0900

    🐛 fix : 예방접종도우미 비밀번호 확인도 제약 조건

--- src/app/signup/more/page.tsx
+++ src/app/signup/more/page.tsx
@@ -100,6 +100,13 @@ export default function Signup(): React.JSX.Element {
     }
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const allowedCharacters = /^[a-zA-Z0-9!@#$%^&*]+$/;
+    if (!allowedCharacters.test(e.key)) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <SignupWrapper>
       <BackHeader title={'예방접종도우미 회원가입'} url={'/signup/terms'} />
@@ -144,12 +151,7 @@ export default function Signup(): React.JSX.Element {
                 ? 'error'
                 : 'default'
             }
-            onKeyDown={(e) => {
-              const allowedCharacters = /^[a-zA-Z0-9!@#$%^&*]+$/;
-              if (!allowedCharacters.test(e.key)) {
-                e.preventDefault();
-              }
-            }}
+            onKeyDown={handleKeyDown}
             onChange={(e) => {
               onChangeValue('password', e.target.value);
             }}
@@ -176,6 +178,7 @@ export default function Signup(): React.JSX.Element {
                 ? 'error'
                 : 'default'
             }
+            onKeyDown={handleKeyDown}
             onChange={(e) => {
               onChangeValue('password_check', e.target.value);
             }}
