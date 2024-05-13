75499bcbc4d5ac5cb6fc90ad219ed27d47674fe3
minjeoong <orange_mj@naver.com>
2024-05-14 02:33:57 +0900

    ♻️ refactor : path.ts 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,10 +10,10 @@ import JoinTemplate from '@/app/_component/temp/JoinTemplate';
 import BottomButton from '@/app/_component/atom/BottomButton';
 import { OnChangeValueType } from '@/types/globalType';
 import * as queryString from 'querystring';
-import { router, useRouter } from 'next/navigation';
-import { useQueryParams } from '@/hooks/useParam';
+import { useRouter } from 'next/navigation';
 import { fetchAccessToken } from '@/hooks/useKakaoLogin';
 import { LocalStorage } from '@/hooks/useUtil';
+import { PATH } from '@/routes/path';
 
 export default function Signup(): React.JSX.Element {
   const router = useRouter();
@@ -28,9 +28,9 @@ export default function Signup(): React.JSX.Element {
   };
   const handleClick = () => {
     if (params.signupState === true) {
-      router.push('/login/helper');
+      router.push(PATH.LOGIN);
     } else {
-      router.push(`/signup/terms`);
+      router.push(PATH.SIGNUP_TERMS);
     }
   };
 
@@ -47,13 +47,12 @@ export default function Signup(): React.JSX.Element {
     const fetchData = async () => {
       if (code) {
         const response = await fetchAccessToken(code);
-        console.log(response);
         if (response?.accessToken) {
           LocalStorage.setItem('accessToken', response.accessToken);
         }
         if (response?.data.member.role === 'ROLE_USER') {
           LocalStorage.setItem('accessToken', response.accessToken);
-          router.push('/home');
+          router.push(PATH.HOME);
         }
       }
     };
---
+++ src/routes/path.ts
@@ -0,0 +1,25 @@
+export const PATH = {
+  root: '/',
+  HOME: '/home',
+  // signup
+  SIGNUP: '/signup',
+  SIGNUP_TERMS: '/signup/terms', // 약관동의
+  SIGNUP_MORE: '/signup/more', // 예방접종도우미 회원가입 - 아이디 비번
+  SIGNUP_INFO: '/signup/info', // 예방접종도우미 회원가입 - 개인정보
+  SIGNUP_CAPCHA: '/signup/captcha', // 예방접종도우미 회원가입 - 보안문자
+  SIGNUP_VERIF: '/signup/verification', // 예방접종도우미 회원가입 - 문자
+  SIGNUP_DONE: '/signup/done', // 예방접종도우미 회원가입 - 완료
+  SIGNUP_IDENTITY: '/signup/moreidentity', // 주민등록번호
+  // login
+  LOGIN: '/login/helper', // 예방접종도우미 로그인
+  LOGIN_FIND: '/login/find', // 아이디비번 찾기 - 개인정보
+  LOGIN_MORE: '/login/more', // 아이디비번 찾기 - 아이디 비번
+  LOGIN_CAPCHA: '/login/captcha', // 아이디비번 찾기 - 보안문자
+  LOGIN_VERIF: '/login/verification', // 아이디비번 찾기 - 문자
+  //moreinfo
+  MOREINFO_DIS: '/moreinfo/dis', // 질환
+  MOREINFO_MED: '/moreinfo/med', // 의료기관 종사자
+  MOREINFO_PRE: '/moreinfo/pre', // 임신여부
+  MOREINFO_TRANS: '/moreinfo/trans', // 장기이식
+  MOREINFO_ID: '/moreinfo/id', // 백곰 ID
+};
