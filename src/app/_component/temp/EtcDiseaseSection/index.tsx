f54c08a685a56dc1f60e72eff8d9e6119080dd98
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-05-16 19:53:30 +0900

    🐛  fix : (디자인 QA) 기타 예방 접종 필터 아이콘 크기 재수정

--- src/app/_component/temp/EtcDiseaseSection/index.tsx
+++ src/app/_component/temp/EtcDiseaseSection/index.tsx
@@ -134,8 +134,8 @@ const EtcDiseaseSection = ({ selectedSection }) => {
               : Images.adjustment_selec
           }
           alt="Filter Icon"
-          width={24}
-          height={24}
+          width={32}
+          height={32}
         />
         <Filter
           label="연령 :"
